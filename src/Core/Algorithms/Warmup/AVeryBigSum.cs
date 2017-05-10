485796384afc048166ca68e4b4037d0e69af51d9
Łukasz Rabiec <lukaszrabiec@gmail.com>
2017-05-11 00:16:49 +0200

    Added A Very Big Sum challenge

---
+++ src/Core/Algorithms/Warmup/AVeryBigSum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Warmup
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/a-very-big-sum
+    /// </summary>
+    public class AVeryBigSum
+    {
+        public static void Main()
+        {
+            Console.ReadLine();
+            var tmpArray = Console.ReadLine().Split(' ');
+            var array = Array.ConvertAll(tmpArray, long.Parse);
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
---
+++ tests/Core/Algorithms/Warmup.Tests/AVeryBigSumTest.cs
@@ -0,0 +1,12 @@
+﻿using TestsCore;
+using Xunit.Abstractions;
+
+namespace Warmup.Tests
+{
+    public class AVeryBigSumTest : BaseTest
+    {
+        public AVeryBigSumTest(ITestOutputHelper output) : base(output, AVeryBigSum.Main)
+        {
+        }
+    }
+}
