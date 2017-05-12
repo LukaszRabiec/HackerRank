d75cb104686ff6440fd95ffb03c12d2955cbe593
Łukasz Rabiec <lukaszrabiec@gmail.com>
2017-05-12 19:16:20 +0200

    Added MiniMaxSum challenge

---
+++ src/Core/Algorithms/Warmup/MiniMaxSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Warmup
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/mini-max-sum
+    /// </summary>
+    public class MiniMaxSum
+    {
+        public static void Main()
+        {
+            var tmpArray = Console.ReadLine().Split(' ');
+            var array = Array.ConvertAll(tmpArray, long.Parse);
+
+            var allSums = GetAllSums(array);
+
+            Console.WriteLine($"{allSums.Min()} {allSums.Max()}");
+        }
+
+        private static long[] GetAllSums(long[] array)
+        {
+            var sums = new long[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sums[i] = SumWithoutElement(array, i);
+            }
+
+            return sums;
+        }
+
+        private static long SumWithoutElement(long[] array, int elementIndex)
+        {
+            return array.Sum() - array[elementIndex];
+        }
+    }
+}
---
+++ tests/Core/Algorithms/Warmup.Tests/MiniMaxSumTest.cs
@@ -0,0 +1,12 @@
+﻿using TestsCore;
+using Xunit.Abstractions;
+
+namespace Warmup.Tests
+{
+    public class MiniMaxSumTest : BaseTest
+    {
+        public MiniMaxSumTest(ITestOutputHelper output) : base(output, MiniMaxSum.Main)
+        {
+        }
+    }
+}
