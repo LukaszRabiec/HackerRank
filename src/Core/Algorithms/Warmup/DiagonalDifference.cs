d5efa60924e6bcdb3d8327224ca9a4fdaf401f0a
Łukasz Rabiec <lukaszrabiec@gmail.com>
2017-05-12 17:52:09 +0200

    Added Diagonal Difference challenge

---
+++ src/Core/Algorithms/Warmup/DiagonalDifference.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Warmup
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/diagonal-difference
+    /// </summary>
+    public class DiagonalDifference
+    {
+        public static void Main()
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var array = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                var aTmp = Console.ReadLine().Split(' ');
+                array[i] = Array.ConvertAll(aTmp, int.Parse);
+            }
+
+            var sumOfDiagonals = GetSumOfDiagonals(array);
+
+            Console.WriteLine(Math.Abs(sumOfDiagonals));
+        }
+
+        private static int GetSumOfDiagonals(int[][] array)
+        {
+            int firstSum = 0;
+            int secondSum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                var lastElement = array[i].Length - 1 - i;
+                firstSum += array[i][i];
+                secondSum += array[i][lastElement];
+            }
+
+            return firstSum - secondSum;
+        }
+    }
+}
---
+++ tests/Core/Algorithms/Warmup.Tests/DiagonalDifferenceTest.cs
@@ -0,0 +1,12 @@
+﻿using TestsCore;
+using Xunit.Abstractions;
+
+namespace Warmup.Tests
+{
+    public class DiagonalDifferenceTest : BaseTest
+    {
+        public DiagonalDifferenceTest(ITestOutputHelper output) : base(output, DiagonalDifference.Main)
+        {
+        }
+    }
+}
